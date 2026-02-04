3c74e5bb5aa728d9e66998794edd3131c9fa4d40
Lomtya <matveev.kostik@gmail.com>
2026-02-04 14:40:15 +0700

    week 1 practice 2: Added about page with template, navigation and dynamic data

--- gallery/views.py
+++ gallery/views.py
@@ -14,3 +14,23 @@ def home(request):
     'assets': fake_database, # Передаем весь список
     }
     return render(request, 'gallery/index.html', context_data)
+
+
+def about(request):
+    # Подготовка данных для шаблона
+    context = {
+        'page_title': 'О проекте - 3D Хранилище',
+        'author_name': 'Ваше Имя',  # Замените на своё имя
+        'course_name': 'Web Структуры',
+        'project_description': 'Это учебный проект для хранения и управления 3D-моделями.',
+        'technologies': [
+            'Django 5.x',
+            'Python 3.x',
+            'HTML/CSS',
+            'SQLite',
+            'Django Templates'
+        ],
+        'current_year': 2024  # Можно динамически вычислять
+    }
+    
+    return render(request, 'gallery/about.html', context)
